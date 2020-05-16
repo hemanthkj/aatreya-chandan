7a845134c2739ffbaf6e859f790f57d0ed9f0295
hemanthkj <46856927+hemanthkj@users.noreply.github.com>
2020-05-16 13:50:58 +0530

    Update data.js

--- assets/javascripts/data.js
+++ assets/javascripts/data.js
@@ -1,11 +1,16 @@
 var settings = {
-  "url": "https://2019ncov.asia/api/confirmed",
-  "method": "GET",
-  "timeout": 0,
-};
+    'cache': false,
+    'dataType': "jsonp",
+    "async": true,
+    "crossDomain": true,
+    "url": "https://2019ncov.asia/api/confirmed",
+    "method": "GET",
+    "headers": {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Access-Control-Allow-Origin":"*"
+    }
+}
 
 $.ajax(settings).done(function (response) {
-  console.log(response);
-  $("#api_data").innerText = JSON.stringify(response)
+    console.log(response);
 });
- 
