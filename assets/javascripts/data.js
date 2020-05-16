c3b92a448992f14299dcadbd4e2e8a6750075d36
hemanthkj <46856927+hemanthkj@users.noreply.github.com>
2020-05-16 14:01:08 +0530

    Update data.js

--- assets/javascripts/data.js
+++ assets/javascripts/data.js
@@ -1,14 +1,14 @@
 var settings = {
     'cache': false,
-    'dataType': "jsonp",
+    // 'dataType': "*",
     "async": true,
     "crossDomain": true,
-    "url": "https://2019ncov.asia/api/confirmed",
+    "url": "https://api.covid19india.org/travel_history.json",
     "method": "GET",
-    "headers": {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "Access-Control-Allow-Origin":"*"
-    }
+    // "headers": {
+    //     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    //     "Access-Control-Allow-Origin":"*"
+    // }
 }
 
 $.ajax(settings).done(function (response) {
